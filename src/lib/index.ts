7883de70d7c18505362aa85a6178b2deca96fe65
Maxime Rainville <maxime@silverstripe.com>
2021-09-07 10:55:57 +1200

    Track the 4.9 release line

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,29 +1,29 @@
 import { ReleaseLine } from './ReleaseLine';
 import { SSModule } from './SSModule';
 import raw from './data.json';
-import cwp26x from './release-set/cwp/cwp-recipe-kitchen-sink/2.6.x-dev.json';
 import cwp27x from './release-set/cwp/cwp-recipe-kitchen-sink/2.7.x-dev.json';
 import cwp28x from './release-set/cwp/cwp-recipe-kitchen-sink/2.8.x-dev.json';
-import cwp2x from './release-set/cwp/cwp-recipe-kitchen-sink/2.x-dev.json';
-import installer45x from './release-set/silverstripe/installer/4.5.x-dev.json';
-import installer46x from './release-set/silverstripe/installer/4.6.x-dev.json';
 import installer47x from './release-set/silverstripe/installer/4.7.x-dev.json';
 import installer48x from './release-set/silverstripe/installer/4.8.x-dev.json';
+import installer49x from './release-set/silverstripe/installer/4.9.x-dev.json';
 import installer4x from './release-set/silverstripe/installer/4.x-dev.json';
+import sink49x from './release-set/silverstripe/recipe-kitchen-sink/4.9.x-dev.json';
+import sink4x from './release-set/silverstripe/recipe-kitchen-sink/4.x-dev.json';
 
 
 
 const data: readonly SSModule[] = raw;
 const releases: {readonly [release: string]: ReleaseLine} = {
   'Silverstripe CMS 4.x': installer4x,
+  'Silverstripe CMS 4.9.x': installer49x,
   'Silverstripe CMS 4.8.x': installer48x,
   'Silverstripe CMS 4.7.x': installer47x,
-  'Silverstripe CMS 4.6.x': installer46x,
-  'Silverstripe CMS 4.5.x': installer45x,
-  'CWP 2.x': cwp2x,
+
+  'Silverstripe Sink 4.x': sink4x,
+  'Silverstripe Sink 4.9.x': sink49x,
   'CWP 2.8.x': cwp28x,
   'CWP 2.7.x': cwp27x,
-  'CWP 2.6.x': cwp26x,
+
 }
 
 export {
